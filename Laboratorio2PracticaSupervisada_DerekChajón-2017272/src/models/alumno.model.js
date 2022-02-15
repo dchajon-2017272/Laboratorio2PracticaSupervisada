3d46ee1b4aaa3c112686f0f6084f7f00f9092603
dchajon-2017272 <99756568+dchajon-2017272@users.noreply.github.com>
2022-02-15 12:14:50 -0600

    Add files via upload

---
+++ Laboratorio2PracticaSupervisada_DerekChajón-2017272/src/models/alumno.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlumnoSchema = Schema({
+    nombre: String,
+    apellido: String,
+    email: String,
+    usuario: String,
+    password: String,
+    rol: String
+    //cursos: String
+});
+
+module.exports = mongoose.model('Alumnos', AlumnoSchema);
---
+++ Laboratorio2PracticaSupervisada_DerekChajón-2017272/src/routes/alumno.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const alumnoControlador = require('../controllers/alumno.controller');
+const md_autenticacion = require('../middlewares/autenticacion');
+
+const api = express.Router();
+
+api.post('/registrarAlumno', alumnoControlador.Registrar);
+api.post('/loginAlumno', alumnoControlador.Login);
+api.put('/editarAlumno/:idAlumno', md_autenticacion.Auth,alumnoControlador.EditarPerfil);
+api.delete('/eliminarAlumno/:idAlumno',md_autenticacion.Auth,alumnoControlador.EliminarPerfil)
+
+module.exports = api;
